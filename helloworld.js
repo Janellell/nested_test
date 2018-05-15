4792e751b68d56bc29704e2c2966ae94ebcd2beb
Michiel Cuijpers <m.cuijpers@sig.eu>
2018-05-15 16:35:12 +0200

    Create helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,19 @@
+public class HelloWorld {
+	
+	public  int add(int first, int second){
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		int third = first+second;
+		
+		return third;
+	}
+
+}
